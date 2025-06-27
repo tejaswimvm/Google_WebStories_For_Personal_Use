144434f203544cb984ae72d1a39ee32de43acd21
tejaswimvm <tejaswi.mvmbs@gmail.com>
2025-06-27 11:06:43 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/webstories/", // trailing slash is safer for asset loading
+  base: "/webstories", // trailing slash is safer for asset loading
   resolve: {
     alias: {
       src: path.resolve("./src"),
